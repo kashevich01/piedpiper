f99c5b2d8f6fa726c602b1999c8dfcddabbe1009
Vlados <78566980+kashevich01@users.noreply.github.com>
2021-02-22 02:07:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.header_list').click(function(event){
+        $('.header_burger,.header_menu').removeClass('active');
+        $('body').removeClass('lock');
+       }); 
+});
